e5134fe7adf7e764bfd3d6c7767fdf0b0bc5507f
micdech <micdech@gmail.com>
2023-12-22 00:32:26 +0100

    sync

--- src/Illuminate/Foundation/Application.php
+++ src/Illuminate/Foundation/Application.php
@@ -23,6 +23,7 @@ use Symfony\Component\HttpFoundation\Request as SymfonyRequest;
 use Symfony\Component\HttpKernel\Exception\HttpException;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
+use Symfony\Component\HttpFoundation\Response;
 
 class Application extends Container implements ApplicationContract, HttpKernelInterface
 {
